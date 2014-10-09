8358a7615af65da12c93397bf0ab2dd2cf469fe2
Joey Cozza <joeycozza@gmail.com>
2014-10-08 19:11:38 -0600

    adding some email templates

--- controllers/email.js
+++ controllers/email.js
@@ -3,26 +3,59 @@
  */
 var nodemailer = require('nodemailer');
 var secrets = require('../config/secrets.js');
+var emailTemplates = require('swig-email-templates');
+var path = require('path');
+var templateOptions = {
+  root: path.join(__dirname, "../templates")
+};
+
 
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: secrets.emailInfo.user,
-        pass: secrets.emailInfo.pass
-    }
+  service: 'Gmail',
+  auth: {
+    user: secrets.emailInfo.user,
+    pass: secrets.emailInfo.pass
+  }
 });
 
 
 function sendMail(mailOptions, callback) {
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Message sent: ' + info.response);
-            if (typeof callback === 'function')
-                callback();
-        }
-    });
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Message sent: ' + info.response);
+      if (typeof callback === 'function') {
+        callback();
+      }
+    }
+  });
 }
-
 exports.sendEmail = sendMail;
+
+
+exports.landingPage = function (req, res) {
+
+  emailUNN(req.body);
+
+  function emailUNN(body) {
+    emailTemplates(templateOptions, function (err, render) {
+      var context = req.body;
+      render('interestedParty.html', context, function (err, html) {
+        if (err) {
+          console.log('emailTemplates err', err);
+        } else {
+          var emailOptions = {
+            from: 'Utah Node Ninjas <utahnodeninjas@gmail.com>',
+            to: body.email,
+            subject: 'Someone Reaching Out',
+            html: html
+          };
+          sendMail(emailOptions);
+        }
+      });
+    });
+  }
+
+
+};
